7c072623a262f0494a8b9d3eab76030c4305dc65
cheriviane88 <kainacheriviane@gmail.com>
2024-09-06 20:09:43 +0100

    Add files via upload

--- login.cpp
+++ login.cpp
@@ -6,9 +6,51 @@ Login::Login(QWidget *parent)
     , ui(new Ui::Login)
 {
     ui->setupUi(this);
+    appdb = QSqlDatabase::addDatabase("QSQLITE");
+    appdb.setDatabaseName("C:/Users/EUROPEONLINE/Desktop/database/appdb.db");
+    if (!appdb.open()) {
+        qDebug() << "Failed to connect to factdb: " << appdb.lastError().text();
+    }
 }
 
 Login::~Login()
 {
     delete ui;
 }
+
+void Login::on_pushButton_clicked()
+{
+
+        QString username,password;
+        username = ui->lineEdit->text();
+        password = ui->lineEdit_2->text();
+
+        if(!appdb.isOpen()){
+            qDebug()<<"Connections Failed";
+            return;
+        }
+        QSqlQuery qry;
+        if(qry.exec("select * from newdt where name='"+username+"' and password='"+password+"'")){
+
+            int count = 0;
+            while(qry.next()){
+                count++;
+            }
+            if(count==1){
+                ui->label->setText("Correct");
+
+
+
+            }
+            if(count>1)
+                ui->label->setText("Duplicate");
+            if(count<1)
+                ui->label->setText("Not Correct");
+
+        }
+
+    }
+
+
+
+
--- login.h
+++ login.h
@@ -1,7 +1,12 @@
 #ifndef LOGIN_H
 #define LOGIN_H
 
+#include<QtSql>
+#include<QtDebug>
+#include<QFileInfo>
 #include <QMainWindow>
+
+#include<profile.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +22,11 @@ public:
     Login(QWidget *parent = nullptr);
     ~Login();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::Login *ui;
+    QSqlDatabase appdb;
 };
 #endif // LOGIN_H
