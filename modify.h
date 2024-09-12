0bee9bb4645f2986c406899e719089edcc382c93
BG-Emmanuel <tambiemmanuel3@gmail.com>
2024-09-13 00:09:32 +0100

    Update modify.h

--- modify.h
+++ modify.h
@@ -22,3 +22,16 @@ public:
 
 private slots:
     void on_listView_indexesMoved(const QModelIndexList &indexes);
+
+    void on_listView_activated(const QModelIndex &index);
+
+    void on_listView_2_activated(const QModelIndex &index);
+
+private:
+    Ui::Modif *ui;
+    QSqlDatabase appdb;
+    void loadData();
+};
+
+#endif // MODIF_H
+
