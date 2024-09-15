5f71eb758033bb02a073652dd04d87a319235ff6
TIAGHO <tiagho.momo@ictuniversity.edu.cm>
2024-09-15 15:09:59 +0100

    Add files via upload

--- facturation.h
+++ facturation.h
@@ -19,9 +19,12 @@ class Facturation : public QMainWindow
 public:
     Facturation(QWidget *parent = nullptr);
     ~Facturation();
+    void loadData();
 
 private slots:
     void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
 
 private:
     Ui::Facturation *ui;
