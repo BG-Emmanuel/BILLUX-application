78b18c7b15fc49d221337d92b2e33655eb5571fa
TIAGHO <tiagho.momo@ictuniversity.edu.cm>
2024-09-16 12:59:50 +0100

    Add files via upload

--- facturation.cpp
+++ facturation.cpp
@@ -1,5 +1,6 @@
 #include "facturation.h"
 #include "ui_facturation.h"
+#include <QMessageBox>
 
 Facturation::Facturation(QWidget *parent)
     : QDialog(parent)
@@ -8,7 +9,10 @@ Facturation::Facturation(QWidget *parent)
     ui->setupUi(this);
 
     factdb =  QSqlDatabase::addDatabase("QSQLITE");
-    factdb.setDatabaseName("C:/Users/Dell/Desktop/database/itemdb.db");
+    factdb.setDatabaseName("C:/Users/Dell/Desktop/database/factdb.db");
+    itemdb =  QSqlDatabase::addDatabase("QSQLITE");
+    itemdb.setDatabaseName("C:/Users/Dell/Desktop/database/itemdb.db");
+   //loadData();
 
 }
 
@@ -16,34 +20,178 @@ Facturation::~Facturation()
 {
     delete ui;
 }
-/*void Facturation::setLineEdit(const QString &text) {
-    ui->lineEdit_1->setText(text);
-}*/
-void Facturation::setLineEditText(const QString &text)
+/*void Facturation::loadData()
 {
-    ui->lineEdit_1->setText(text);
+    if(!itemdb.open()){
+        qDebug()<<"Connection Failed";
+
+        return;
+    }
+
+    QSqlQueryModel * modal2 = new QSqlQueryModel();
+    QSqlQuery* qry2 = new QSqlQuery(itemdb);
+    qry2->prepare("select designation from itemdt");
+    qry2->exec();
+    modal2->setQuery(*qry2);
+    ui->listView_14->setModel(modal2);
+
+    QSqlQueryModel * modal = new QSqlQueryModel();
+    QSqlQuery* qry = new QSqlQuery(itemdb);
+    qry->prepare("select reference from itemdt");
+    qry->exec();
+    modal->setQuery(*qry);
+    ui->listView_13->setModel(modal);
+}*/
+
+void Facturation::setLineEditText(const QString &text) {
+    ui->lineEdit->setText(text);
 }
+
+void Facturation::on_pushButton_2_clicked()
+{
+
+    int unit = ui->lineEdit_3->text().toInt();
+    int quant = ui->lineEdit_5->text().toInt();
+    int dis = ui->lineEdit_4->text().toInt();
+    int tot = (((unit*quant)/100.0)*dis);
+    int totalPrice = (((unit*quant)-tot));
+
+    QString reference,designation,unitPrice,discount,quantity;
+    reference = ui->lineEdit->text();
+    designation = ui->lineEdit_2->text();
+    unitPrice = ui->lineEdit_3->text();
+    discount = ui->lineEdit_4->text();
+    quantity = ui->lineEdit_5->text();
+
+    if(!factdb.open()){
+        qDebug()<<"Connections Failed";
+
+        return;
+    }
+
+    QSqlQuery qry;
+    qry.prepare("INSERT INTO factdt(reference,designation,unitPrice,discount,quantity,totalPrice) VALUES (:reference,:designation,:unitPrice,:discount,:quantity,:totalPrice)");
+    qry.bindValue(":reference",reference);
+    qry.bindValue(":designation",designation);
+    qry.bindValue(":unitPrice",unitPrice);
+    qry.bindValue(":discount",discount);
+    qry.bindValue(":quantity",quantity);
+    qry.bindValue(":totalPrice",totalPrice);
+
+
+
+
+    if(qry.exec()){
+
+        QMessageBox::critical(this,tr("SAVED"),tr("DATA SAVED"));
+    }
+    else{
+        QMessageBox::critical(this,tr("error"),qry.lastError().text());
+    }
+
+    QSqlQueryModel * modal1 = new QSqlQueryModel();
+    QSqlQuery* qry1 = new QSqlQuery(factdb);
+    qry1->prepare("select reference from factdt");
+    qry1->exec();
+    modal1->setQuery(*qry1);
+    ui->listView->setModel(modal1);
+
+    QSqlQueryModel * modal2 = new QSqlQueryModel();
+    QSqlQuery* qry2 = new QSqlQuery(factdb);
+    qry2->prepare("select designation from factdt");
+    qry2->exec();
+    modal2->setQuery(*qry2);
+    ui->listView_2->setModel(modal2);
+
+    QSqlQueryModel * modal3 = new QSqlQueryModel();
+    QSqlQuery* qry3 = new QSqlQuery(factdb);
+    qry3->prepare("select unitPrice from factdt");
+    qry3->exec();
+    modal3->setQuery(*qry3);
+    ui->listView_3->setModel(modal3);
+
+    QSqlQueryModel * modal4 = new QSqlQueryModel();
+    QSqlQuery* qry4 = new QSqlQuery(factdb);
+    qry4->prepare("select discount from factdt");
+    qry4->exec();
+    modal4->setQuery(*qry4);
+    ui->listView_4->setModel(modal4);
+
+    QSqlQueryModel * modal5 = new QSqlQueryModel();
+    QSqlQuery* qry5 = new QSqlQuery(factdb);
+    qry5->prepare("select quantity from factdt");
+    qry5->exec();
+    modal5->setQuery(*qry5);
+    ui->listView_5->setModel(modal5);
+
+    QSqlQueryModel * modal6 = new QSqlQueryModel();
+    QSqlQuery* qry6 = new QSqlQuery(factdb);
+    qry6->prepare("select totalPrice from factdt");
+    qry6->exec();
+    modal6->setQuery(*qry6);
+    ui->listView_6->setModel(modal6);
+
+   /* QSqlQuery query;
+    query.prepare("SELECT SUM(totalPrice) FROM factdt");
+
+    if (query.exec() && query.next()) {
+        // Retrieve the sum from the query result
+        int sum = query.value(0).toInt();
+
+        // Display the sum on the QLabel
+        ui->label_7->setText("Sum: " + QString::number(sum));
+    } else {
+        qDebug() << "Failed to execute the query: " << query.lastError().text();
+    }*/
+
+
+
+
+    ui->lineEdit->clear();
+    ui->lineEdit_2->clear();
+    ui->lineEdit_3->clear();
+    ui->lineEdit_4->clear();
+    ui->lineEdit_5->clear();
+}
+
+
 void Facturation::on_pushButton_clicked()
 {
-    if (!factdb.open()) {
-        qDebug() << "Failed to connect to the database:" << factdb.lastError();
-    }
-    QSqlQuery query;
-    QString columnName = "totalPrice";  // Replace with your actual column name
-    QString tableName = "factdt";    // Replace with your actual table name
 
-    // Construct the SQL query
-    QString queryString = QString("SELECT SUM(%1) FROM %2").arg(columnName).arg(tableName);
 
-    if (query.exec(queryString)) {
-        if (query.next()) {
-            double sum = query.value(5).toDouble();  // Get the sum from the result
-            ui->label->setText(QString::number(sum));  // Set the sum to the label
-        }
-    } else {
-        // Handle SQL error
-        qDebug() << "Query failed:" << query.lastError();
-        ui->label->setText("Error");
-    }
+
+    Item item;
+    item.setModal(true);
+    item.exec();
+
+
+}
+
+
+
+
+
+void Facturation::on_pushButton_3_clicked()
+{
+    AddItem additem;
+    additem.setModal(true);
+    additem.exec();
+}
+/*Facturation::lineEdit_1(){
+    return ui->lineEdit;
+}*/
+
+
+void Facturation::on_pushButton_4_clicked()
+{
+    Newuser newuser;
+    newuser.setModal(true);
+    newuser.exec();
+}
+
+
+void Facturation::on_pushButton_5_clicked()
+{
+
 }
 
--- facturation.h
+++ facturation.h
@@ -1,12 +1,13 @@
 #ifndef FACTURATION_H
 #define FACTURATION_H
-
 #include<QtSql>
 #include<QtDebug>
 #include<QFileInfo>
 #include <QDialog>
-#include <QPushButton>
+#include<additem.h>
 #include<item.h>
+
+#include<newuser.h>
 
 namespace Ui {
 class Facturation;
@@ -21,13 +22,21 @@ public:
     ~Facturation();
     void setLineEditText(const QString &text);
 
-
 private slots:
+    void on_pushButton_2_clicked();
+
     void on_pushButton_clicked();
 
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_5_clicked();
 
 private:
     Ui::Facturation *ui;
+    void loadData();
+    QSqlDatabase itemdb;
     QSqlDatabase factdb;
 };
 
